10aaa2aa988ec8223f5e9a7e230bfa4e99d1137c
Jack Frederick <jfred@Jacks-Air-100.lan>
2024-11-05 20:36:08 -0600

    General Adjustments

--- script.js
+++ script.js
@@ -12,25 +12,22 @@ function setActiveButton() {
     });
 }
 
-function toggleImages() {
+function toggleImages(event) {
     const image1 = document.getElementById("image1");
     const singleImage = document.getElementById("single-image");
-
     
-    if (image1.classList.contains("hidden")) {
-        
+    if (event.target.id === "left-arrow") {
+
         image1.classList.remove("hidden");
         singleImage.classList.add("hidden");
-    } else {
-        
+    } else if (event.target.id === "right-arrow") {
+
         image1.classList.add("hidden");
         singleImage.classList.remove("hidden");
     }
 }
 
-
-document.getElementById("left-arrow").addEventListener("click", toggleImages);
-document.getElementById("right-arrow").addEventListener("click", toggleImages);
-
+document.getElementById("left-arrow")?.addEventListener("click", toggleImages);
+document.getElementById("right-arrow")?.addEventListener("click", toggleImages);
 
 window.onload = setActiveButton;
